b77a57891d980b7a4f2aaea1b531151f7030102b
何建新 <yiyuanhanly@126.com>
2017-11-14 13:16:35 +0800

    123

---
+++ FF14Raiders/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FF14Raiders
+//
+//  Created by 何建新 on 2017/11/14.
+//  Copyright © 2017年 何建新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
